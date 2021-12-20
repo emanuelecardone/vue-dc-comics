9430bef9caab8e7672abfb355b5a2672450af088
emanuelecardone <e_m.cardone@yahoo.it>
2021-12-20 17:27:45 +0100

    Fatto footer superiore

--- src/components/UpperFooter.vue
+++ src/components/UpperFooter.vue
@@ -2,10 +2,19 @@
     <section class="upper_footer_wrapper">
         <div class="my_container">
             <div class="upper_footer_subwrapper left">
-                test
+                <div v-for="(list, listIndex) in footerLists" :key="listIndex" class="footer_list_wrapper" :class="{'fixed_footer_up_height': listIndex === 0, 'fixed_footer_down_height': listIndex === 1, 'fixed_footer_full_height': listIndex > 1}">
+                    <div v-if="footerLists.length > 0" class="footer_list_subwrapper">
+                        <h2>{{list.title}}</h2>
+                        <ul>
+                            <li v-for="(link, linkIndex) in footerLists[listIndex].links" :key="linkIndex">
+                                <a :href="link.url">{{link.name}}</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div class="upper_footer_subwrapper right">
-                test
+                
             </div>
         </div>
     </section>
@@ -13,7 +22,134 @@
 
 <script>
 export default {
-    name: 'UpperFooter'
+    name: 'UpperFooter',
+    data: function(){
+        return {
+            numberOfCols: 3,
+            footerLists: [
+                {
+                    title: 'dc comics',
+                    links: [
+                        {
+                            name: 'Characters',
+                            url: '#'
+                        },
+                        {
+                            name: 'Comics',
+                            url: '#'
+                        },
+                        {
+                            name: 'Movies',
+                            url: '#'
+                        },
+                        {
+                            name: 'TV',
+                            url: '#'
+                        },
+                        {
+                            name: 'Games',
+                            url: '#'
+                        },
+                        {
+                            name: 'Videos',
+                            url: '#'
+                        },
+                        {
+                            name: 'News',
+                            url: '#'
+                        }
+                    ]
+                },
+                {
+                    title: 'shop',
+                    links: [
+                        {
+                            name: 'Shop DC',
+                            url: '#'
+                        },
+                        {
+                            name: 'Shop DC Collectibles',
+                            url: '#'
+                        }
+                    ]
+                },
+                {
+                    title: 'dc',
+                    links: [
+                        {
+                            name: 'Terms Of Use',
+                            url: '#'
+                        },
+                        {
+                            name: 'Privacy policy (New)',
+                            url: '#'
+                        },
+                        {
+                            name: 'Ad Choices',
+                            url: '#'
+                        },
+                        {
+                            name: 'Advertising',
+                            url: '#'
+                        },
+                        {
+                            name: 'Jobs',
+                            url: '#'
+                        },
+                        {
+                            name: 'Subscriptions',
+                            url: '#'
+                        },
+                        {
+                            name: 'Talent Workshops',
+                            url: '#'
+                        },
+                        {
+                            name: 'CPSC Certificates',
+                            url: '#'
+                        },
+                        {
+                            name: 'Ratings',
+                            url: '#'
+                        },
+                        {
+                            name: 'Shop Help',
+                            url: '#'
+                        },
+                        {
+                            name: 'Contact Us',
+                            url: '#'
+                        }
+                    ]
+                },
+                {
+                    title: 'sites',
+                    links: [
+                        {
+                            name: 'DC',
+                            url: '#'
+                        },
+                        {
+                            name: 'MAD Magazine',
+                            url: '#'
+                        },
+                        {
+                            name: 'DC Kids',
+                            url: '#'
+                        },
+                        {
+                            name: 'DC Universe',
+                            url: '#'
+                        },
+                        {
+                            name: 'DC Power Visa',
+                            url: '#'
+                        }
+                    ]
+                },
+            ]
+        };
+    }
 }
 </script>
 
@@ -29,11 +165,40 @@ export default {
         .my_container{
             height: 100%;
             display: flex;
+            justify-content: space-between;
 
             .upper_footer_subwrapper{
-                width: 50%;
+                width: 40%;
                 height: 100%;
 
+                &.left{
+                    padding: 3rem 0;
+                    display: flex;
+                    flex-direction: column;
+                    flex-wrap: wrap;
+
+                    .footer_list_wrapper{
+
+                        &.fixed_footer_up_height{
+                            height: 65%;
+                        }
+                        &.fixed_footer_down_height{
+                            height: 35%;
+                        }
+                        &.fixed_footer_full_height{
+                            height: 100%;
+                        }
+                        h2{
+                            color: white;
+                            text-transform: uppercase;
+                            margin-bottom: 20px;
+                        }
+                        a{
+                            color: $secondary_color;
+                            line-height: 25px;
+                        }
+                    }   
+                }
                 &.right{
                     background-image: url('../assets/img/dc-logo-bg.png');
                     @include common_bg;
