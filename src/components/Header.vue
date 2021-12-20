a9581af580dc9e1ee9c882097c9059fae1745d38
emanuelecardone <e_m.cardone@yahoo.it>
2021-12-20 14:26:24 +0100

    Fatto Header

---
+++ src/components/Header.vue
@@ -0,0 +1,112 @@
+<template>
+    <header>
+        <img :src="require('../assets/img/' + logoSrc)" alt="Logo DC">
+
+        <ul v-if="headerLinks.length > 0">
+            <li v-for="(link, index) in headerLinks" :key="index" :class="{'current': index === currentItem}">
+                <a href="#">{{link.text}}</a> 
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data: function(){
+        return {
+            logoSrc: 'dc-logo.png',
+            currentItem: 1,
+            headerLinks: [
+                {   
+                    text: 'characters',
+                    current: false,
+                    url: '#'
+                },
+                {   
+                    text: 'comics',
+                    current: true,
+                    url: '#'
+                },
+                {   
+                    text: 'movies',
+                    current: false,
+                    url: '#'
+                },
+                {   
+                    text: 'tv',
+                    current: false,
+                    url: '#'
+                },
+                {   
+                    text: 'games',
+                    current: false,
+                    url: '#'
+                },
+                {   
+                    text: 'collectibles',
+                    current: false,
+                    url: '#'
+                },
+                {   
+                    text: 'videos',
+                    current: false,
+                    url: '#'
+                },
+                {   
+                    text: 'fans',
+                    current: false,
+                    url: '#'
+                },
+                {   
+                    text: 'news',
+                    current: false,
+                    url: '#'
+                },
+                {   
+                    text: 'shop',
+                    current: false,
+                    url: '#'
+                }
+            ]
+        };
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+@import '../style/mixins.scss';
+
+    header{
+        height: 120px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        img{
+            width: 80px;
+        }
+
+        ul{ 
+            @include inline_ul;
+            gap: 1.5rem;
+
+            li{
+                line-height: 115px;
+                align-self: flex-start;
+                &.current{
+                    border-bottom: 5px solid $primary_color;
+                    a{
+                        color: $primary_color;
+                    }
+                }
+
+                a{
+                    color: $secondary_color;                    
+                }
+            }
+        }
+    }
+
+</style>
