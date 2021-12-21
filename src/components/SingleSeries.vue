085ccf736fbfb085b18a403f2794c45704d78834
emanuelecardone <e_m.cardone@yahoo.it>
2021-12-21 13:50:24 +0100

    Messa la base delle single series e ciclate

---
+++ src/components/SingleSeries.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="single_series_wrapper p_10">
+        <img :src="seriesObject.thumb" :alt="'Immagine di ' + seriesObject.series">
+        <h3 class="mt_10 fs_15">{{seriesObject.series}}</h3>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'SingleSeries',
+    props: ['seriesObject']
+}
+</script>
+
+<style lang="scss" scoped>
+    .single_series_wrapper{
+        width: calc(100% / 6);
+        text-transform: uppercase;
+        color: white;
+    }
+</style>
